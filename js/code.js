7f1b4611b73d0d9332917242837ec2f59b674c85
Ira <iritmeg@gmail.com>
2019-11-04 10:54:37 +0200

    Защита 3
    Защита 3

--- js/code.js
+++ js/code.js
@@ -1,26 +1,51 @@
  var buttonSearch = document.querySelector(".hotel-search");
 
  var newForm = document.querySelector(".search");
- 
 
- var newSubmit = document.querySelector(".submit-button");
- console.log(newSubmit);
-
+ var isClose; 
  
+ var myForm = document.querySelector(".form");
+
+ var arrivalDate = document.querySelector(".arrival-date");
+
+ var departureDate = document.querySelector(".departure-date");
+
+
+
  buttonSearch.addEventListener("click", function(evt){
- 	newForm.classList.toggle('hidden-form');
+
+ 	isClose = newForm.classList.contains("hidden-form");
+
+ 	if (isClose==false){
+
+ 		newForm.classList.remove("submit-animation");
+ 	}else {	
+
+ 		newForm.classList.add("submit-animation");
+
+ 	}
+
+ 	newForm.classList.toggle("hidden-form");
  });
 
- newSubmit.addEventListener("mouseover", function(evt){
- 	console.log("over");
- 	newSubmit.classList.add('submit-animation');
 
- 	setTimeout(function(){
- 		newSubmit.classList.remove('submit-animation')
- 		
- 	}, 3000);
 
+ myForm.addEventListener("submit",function(evt) {
+
+ 	if (!arrivalDate.value || !departureDate.value ){	
+
+ 		newForm.classList.remove("submit-animation");
+ 		newForm.classList.add("submit-error");
+
+ 		setTimeout(function(){
+
+ 			newForm.classList.remove("submit-error");
+
+ 		},3000);
+
+ 		evt.preventDefault();	
+ 	} 
+ 	
  });
-
 
 
